3567cb180d4986cf63c46a459e01809ff4a70ec2
caohm <caohongming001@gmail.com>
2019-04-27 20:16:35 +0800

    elastic

---
+++ elastic/create.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+version="7.0.0"
+elasticsearch="elasticsearch"
+kibana="kibana"
+logstash="logstash"
+filebeat="filebeat"
+
+mkdir -p $elasticsearch/${version}
+cat <<EOF > $elasticsearch/${version}/Dockerfile 
+FROM docker.elastic.co/$elasticsearch/$elasticsearch:${version}
+EOF
+
+mkdir -p beats/${filebeat}/${version}
+cat <<bbb > beats/${filebeat}/${version}/Dockerfile
+FROM docker.elastic.co/beats/${filebeat}:${version}
+bbb
+
+mkdir -p ${kibana}/${version}
+cat <<ccc > ${kibana}/${version}/Dockerfile
+FROM docker.elastic.co/${kibana}/${kibana}:${version}
+ccc
+
+mkdir -p ${logstash}/${version}
+cat <<eee > ${logstash}/${version}/Dockerfile
+FROM docker.elastic.co/${logstash}/${logstash}:${version}
+eee
